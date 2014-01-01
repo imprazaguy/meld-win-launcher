2e47723f1b175c7ea4a43cd5110ac76d6d4691c2
Guan-Zhong Huang <imprazaguy@gmail.com>
2014-01-01 13:54:33 +0800

    Conform to C89 standard to correctly compile with MSVC.

--- meld.c
+++ meld.c
@@ -10,12 +10,12 @@ static wchar_t meld_bin[MAX_PATH + 10];
 
 static wchar_t **get_argv()
 {
-    wchar_t **argv_;
+    wchar_t **old_argv, **argv;
     int argc;
     int i;
 
-    argv_ = CommandLineToArgvW(GetCommandLineW(), &argc);
-    if(argv_ == NULL) {
+    old_argv = CommandLineToArgvW(GetCommandLineW(), &argc);
+    if(old_argv == NULL) {
         exit(1);
     }
 
@@ -25,11 +25,11 @@ static wchar_t **get_argv()
     PathRemoveFileSpecW(meld_bin);
     wcscat(meld_bin, L"\\bin\\meld");
 
-    wchar_t **argv = (wchar_t **) malloc(sizeof (*argv) * (argc + 2));
-    argv[0] = argv_[0];
+    argv = (wchar_t **) malloc(sizeof (*argv) * (argc + 2));
+    argv[0] = old_argv[0];
     argv[1] = meld_bin;
     for(i = 1; i < argc; ++i) {
-        argv[i + 1] = argv_[i];
+        argv[i + 1] = old_argv[i];
     }
     argv[argc + 1] = NULL;
     return argv;
@@ -37,20 +37,23 @@ static wchar_t **get_argv()
 
 static wchar_t **get_envp()
 {
-    wchar_t *env_block = GetEnvironmentStringsW();
+    int i, n;
+    wchar_t *env_block, *p;
+    wchar_t **envp;
+
+    env_block = GetEnvironmentStringsW();
     if (env_block == NULL) {
         exit(1);
     }
 
-    int n = 0;
-    wchar_t *p = env_block;
+    n = 0;
+    p = env_block;
     while (*p != L'\0') {
         ++n;
         p += wcslen(p) + 1;
     }
 
-    wchar_t **envp = (wchar_t **) malloc(sizeof (*envp) * n + 1);
-    int i;
+    envp = (wchar_t **) malloc(sizeof (*envp) * n + 1);
     envp[0] = env_block;
     for (i = 1; i < n; ++i) {
         envp[i] = envp[i - 1] + wcslen(envp[i - 1]) + 1;
@@ -61,15 +64,18 @@ static wchar_t **get_envp()
 
 #ifdef USE_WINMAIN
 #define PYTHON_EXE L"pythonw.exe"
-int WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 #else
 #define PYTHON_EXE L"python.exe"
 int main(int _argc, char *_argv[])
 #endif
 {
-    const wchar_t * const *argv = (const wchar_t * const *) get_argv();
+    const wchar_t * const *argv;
+    const wchar_t * const *envp;
+
+    argv = (const wchar_t * const *) get_argv();
     _putenv("LANG=en_US");
-    const wchar_t * const *envp = (const wchar_t * const *) get_envp();
+    envp = (const wchar_t * const *) get_envp();
     _wexecvpe(PYTHON_EXE, argv, envp);
     return 0;
 }
